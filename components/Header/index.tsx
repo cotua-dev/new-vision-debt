0c2a0990ff86157352de7ed33acba853db2e90de
Jeffrey Berry <jeffreyberry93@gmail.com>
2022-09-09 13:07:01 -0700

    fix(index.tsx): use the right attribute name and add twitter tags

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -11,17 +11,23 @@ export function Header(props: HeaderProps) {
             {typeof props.description !== 'undefined' &&
                 <>
                     <meta name="description" content={props.description}/>
-                    <meta name="og:description" content={props.description}/>
+                    <meta property="og:description" content={props.description}/>
+                    <meta property="twitter:description" content={props.description}/>
                 </>
             }
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            <meta name="og:title" content={props.title ? props.title : 'New Vision Debt'}/>
-            <meta name="og:type" content="website"/>
-            <meta name="og:url" content="https://www.newvisiondebtco.com"/>
-            <meta name="og:image" content="https://www.newvisiondebtco.com/images/seo/home.jpg"/>
-            <meta name="og:image:alt" content="Person working on debt"/>
-            <meta name="og:locale" content="en_US"/>
-            <meta name="og:site_name" content="New Vision Debt"/>
+            <meta property="og:title" content={props.title ? props.title : 'New Vision Debt'}/>
+            <meta property="og:type" content="website"/>
+            <meta property="og:url" content="https://www.newvisiondebtco.com"/>
+            <meta property="og:image" content="https://www.newvisiondebtco.com/images/seo/home.jpg"/>
+            <meta property="og:image:alt" content="Person working on debt"/>
+            <meta property="og:locale" content="en_US"/>
+            <meta property="og:site_name" content="New Vision Debt"/>
+            <meta property="twitter:card" content="summary_large_image"/>
+            <meta property="twitter:domain" content="newvisiondebtco.com"/>
+            <meta property="twitter:url" content="https://www.newvisiondebtco.com"/>
+            <meta property="twitter:title" content={props.title ? props.title : 'New Vision Debt'}/>
+            <meta property="twitter:image" content="https://www.newvisiondebtco.com/images/seo/home.jpg"/>
             <link rel="icon" type="image/png" sizes="32x32" href="/images/icons/favicon-32x32.png"/>
             <link rel="icon" type="image/png" sizes="180x180" href="/images/icons/apple-icon-180x180.png"/>
             <link rel="apple-touch-icon" href="/images/icons/android-icon-192x192.png"/>
